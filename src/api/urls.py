4be00d4bb9f82617666aa4818b625f7b87a35f0c
PabloRSantos <pablorsantos15@gmail.com>
2021-02-09 11:43:10 -0300

    feat: swagger

--- src/api/urls.py
+++ src/api/urls.py
@@ -5,9 +5,23 @@ from django.contrib import admin
 from django.urls import path, include
 from api.router import router
 from rest_framework.authtoken.views import obtain_auth_token
+from drf_yasg import openapi
+from rest_framework.permissions import AllowAny
+from drf_yasg.views import get_schema_view
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title='API',
+        default_version='0.1.0',
+        description='API de pontos turisticos'
+    ),
+    public=False,
+    permission_classes=(AllowAny,)
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
     path('api-token-auth/', obtain_auth_token),
+    path('docs', schema_view.with_ui('swagger', cache_timeout=0))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
